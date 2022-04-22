614a8ac4c6d8782d1f0a7ff5aa7330cb111011d1
Nezort11 <nezort11@gmail.com>
2022-04-23 01:14:34 +0300

    Add additional Spotlight Elements API docs

--- {{cookiecutter.project_slug}}/server/config/doc_urls.py
+++ {{cookiecutter.project_slug}}/server/config/doc_urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path("swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger"),
+    path("docs/", TemplateView.as_view(template_name="docs/index.html"), name="docs"),
 ]
